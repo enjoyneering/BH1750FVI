ee16ace7d149f0c7eff262f700191518ba83cfab
enjoyneering <enjoyneering79@gmail.com>
2017-11-06 18:47:44 -0500

    Update BH1750FVI_Demo.ino

--- examples/BH1750FVI_Demo/BH1750FVI_Demo.ino
+++ examples/BH1750FVI_Demo/BH1750FVI_Demo.ino
@@ -1,28 +1,29 @@
-/**************************************************************************/
+/***************************************************************************************************/
 /* 
-  Example of ROHM BH1750FVI Ambient Light Sensor library usage.
+  Example for ROHM BH1750FVI Ambient Light Sensor library
   
-  These sensor uses I2C to communicate. Two pins are required to  
-  interface.
+  written by : enjoyneering79
+  sourse code: https://github.com/enjoyneering/
 
-  Connect BH1750FVI to pins:  SDA     SCL
-  Uno, Mini, Pro:             A4      A5
-  Mega2560, Due:              20      21
-  Leonardo:                   2       3
-  Atiny85:                    0/PWM   2/A1   (TinyWireM)
-  NodeMCU 1.0:                D1/ANY  D2/ANY (D1 & D2 by default)
-  ESP8266 ESP-01:             ANY     ANY
-  
+  Wide range:           1 - 65'535 lx (by default)
+  Possible to detect:   0.11 - 100'000 lx by changing Sensitivity
+
+  This sensor uses I2C bus to communicate, specials pins are required to interface
+
+  Connect chip to pins:    SDA        SCL
+  Uno, Mini, Pro:          A4         A5
+  Mega2560, Due:           20         21
+  Leonardo:                2          3
+  ATtiny85:                0(5)       2/A1(7)   (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
+                                                 & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
+  ESP8266 ESP-01:          GPIO0/D5   GPIO2/D3  (ESP8266Core - https://github.com/esp8266/Arduino)
+  NodeMCU 1.0:             GPIO4/D2   GPIO5/D1
+  WeMos D1 Mini:           GPIO4/D2   GPIO5/D1
+
   BSD license, all text above must be included in any redistribution
 */
-/**************************************************************************/
-#if defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny26__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny45__) || (__AVR_ATtiny84__) || defined(__AVR_ATtiny85__)
- #include <TinyWireM.h>
- #define Wire TinyWireM
-#else defined
- #include <Wire.h>
-#endif
-
+/***************************************************************************************************/
+#include <Wire.h>
 #include <BH1750FVI.h>
 
 /*
@@ -37,95 +38,75 @@ BH1750_CONTINUOUS_HIGH_RES_MODE   - Continuous measurement. Resolution - 1.0 lx.
 BH1750_CONTINUOUS_HIGH_RES_MODE_2 - Continuous measurement. Resolution - 0.5 lx.
 BH1750_CONTINUOUS_LOW_RES_MODE    - Continuous measurement. Resolution - 4.0 lx.
 
-BH1750_ONE_TIME_HIGH_RES_MODE     - One-time measurement with power down after. Resolution - 1.0 lx.
-BH1750_ONE_TIME_HIGH_RES_MODE_2   - One-time measurement with power down after. Resolution - 0.5 lx.
-BH1750_ONE_TIME_LOW_RES_MODE      - One-time measurement with power down after. Resolution - 4.0 lx.
+BH1750_ONE_TIME_HIGH_RES_MODE     - One-time measurement(power down after). Resolution - 1.0 lx.
+BH1750_ONE_TIME_HIGH_RES_MODE_2   - One-time measurement(power down after). Resolution - 0.5 lx.
+BH1750_ONE_TIME_LOW_RES_MODE      - One-time measurement(power down after). Resolution - 4.0 lx.
 
 sensitivity:
 NOTE: your value have to be between min. and max.
 1.00 - default Sensitivity
 0.45 - minimum Sensitivity
 3.68 - maximum Sensitivity
-
-DEFAULT
-BH1750FVI(BH1750_DEFAULT_I2CADDR, BH1750_CONTINUOUS_HIGH_RES_MODE_2, 1.00)
 */
+BH1750FVI myBH1750(BH1750_DEFAULT_I2CADDR, BH1750_CONTINUOUS_HIGH_RES_MODE_2, 1.00);
 
-BH1750FVI myBH1750;
 
 void setup()
 {
   Serial.begin(115200);
+  Serial.flush();
 
- #if defined(ARDUINO_ARCH_ESP8266) || (ESP8266_NODEMCU)
-  while (myBH1750.begin(D1, D2) != true)
- #else
   while (myBH1750.begin() != true)
- #endif
   {
-    Serial.println(F(""));
-    Serial.println(F("ROHM BH1750FVI ambient light sensor is not present..."));
+    Serial.println(F("ROHM BH1750FVI ambient light sensor is not present"));
     delay(5000);
   }
-
   Serial.println(F("ROHM BH1750FVI ambient light sensor is present"));
 }
 
+
 void loop()
 {
-  Serial.println(F(""));
-  Serial.println(F("DEMO: Sensitivity - 1.0, Continuous Mode (default settings)"));
-  Serial.print(F("Light level: "));
-  Serial.print(myBH1750.readLightLevel());
-  Serial.println(F(" +-0.5 lx"));
-  Serial.print(F("Power for 555nm wave: "));
-  Serial.print(myBH1750.readLightLevel()/683);
-  Serial.println(F(" Watt/m^2"));
-  Serial.println(F(""));
-  
-  Serial.println(F("DEMO: Manual Power Down"));
-  myBH1750.powerDown();
-  Serial.println(F(""));
-
-  Serial.println(F("DEMO: Sensitivity - 2.0, Continuous Mode"));
+  /* DEMO - 1 */
+  Serial.println(F("DEMO 1: Sensitivity - 1.0, Continuous High Res II Mode"));
+  Serial.print(F("Light level.........: ")); Serial.print(myBH1750.readLightLevel());     Serial.println(F(" +-0.5 lx"));
+  Serial.print(F("Power for 555nm wave: ")); Serial.print(myBH1750.readLightLevel()/683); Serial.println(F(" Watt/m^2"));
+  myBH1750.reset();
+  /* DEMO - 2 */
+  Serial.println(F("DEMO 2: Sensitivity - 2.00, Continuous High Res II Mode"));
   myBH1750.setSensitivity(2);
-  Serial.print(F("Light level: "));
-  Serial.print(myBH1750.readLightLevel());
-  Serial.println(F(" +-0.5 lx"));
-  Serial.println(F(""));
- 
-  Serial.println(F("DEMO: Sensitivity - 0.5, Continuous Mode"));
+
+  Serial.print(F("Light level.........: ")); Serial.print(myBH1750.readLightLevel());     Serial.println(F(" +-0.5 lx"));
+   /* DEMO - 3 */
+  Serial.println(F("DEMO 3: Sensitivity - 0.50, Continuous High Res II Mode"));
   myBH1750.setSensitivity(0.5);
-  Serial.print(F("Light level: "));
-  Serial.print(myBH1750.readLightLevel());
-  Serial.println(F(" +-0.5 lx"));
-  Serial.println(F(""));
-  
-  Serial.println(F("DEMO: Sensitivity - 3.68, Auto Power Down"));
+
+  Serial.print(F("Light level.........: ")); Serial.print(myBH1750.readLightLevel());     Serial.println(F(" +-0.5 lx"));
+  /* DEMO - 4 */
+  Serial.println(F("DEMO 4: Sensitivity - 3.68, Power Down High Res   Mode"));
   myBH1750.setSensitivity(3.68);
   myBH1750.setResolution(BH1750_ONE_TIME_HIGH_RES_MODE);
-  Serial.print(F("Light level: "));
-  Serial.print(myBH1750.readLightLevel());
-  Serial.println(F(" +-1.0 lx"));
-  Serial.println(F(""));
-  
-  
-  Serial.println(F("DEMO: Sensitivity - 0.45, Auto Power Down"));
+
+  Serial.print(F("Light level.........: ")); Serial.print(myBH1750.readLightLevel());     Serial.println(F(" +-1.0 lx")); 
+  /* DEMO - 5 */
+  Serial.println(F("DEMO 5: Sensitivity - 0.45, Power Down High Res II Mode"));
   myBH1750.setSensitivity(0.45);
   myBH1750.setResolution(BH1750_CONTINUOUS_HIGH_RES_MODE_2);
-  Serial.print(F("Light level: "));
-  Serial.print(myBH1750.readLightLevel());
-  Serial.println(F(" +-0.5 lx"));
-  Serial.println(F(""));
-  
-  Serial.println(F("DEMO: Sensitivity - 1.0, Auto Power Down"));
+
+  Serial.print(F("Light level.........: ")); Serial.print(myBH1750.readLightLevel());     Serial.println(F(" +-0.5 lx"));
+  /* DEMO - 6 */
+  Serial.println(F("DEMO 6: Sensitivity - 1.00, Power Down High Res II Mode"));
   myBH1750.setSensitivity(1);
   myBH1750.setResolution(BH1750_ONE_TIME_HIGH_RES_MODE_2);
-  Serial.print(F("Light level: "));
-  Serial.print(myBH1750.readLightLevel());
-  Serial.println(F(" +-0.5 lx"));
-  Serial.println(F(""));
+
+  Serial.print(F("Light level.........: ")); Serial.print(myBH1750.readLightLevel());     Serial.println(F(" +-0.5 lx"));
+  /* DEMO - 7 */ 
+  Serial.println(F("DEMO 7: Manual Power Down"));
+  myBH1750.powerDown();
+  /* DEMO - END */
+  Serial.print(F("DEMO: Starts over again in 5 sec."));
+  delay(5000);
   
-  Serial.print(F("DEMO: Starts over again in 8 sec."));
-  Serial.println(F(""));
-  delay(8000);
+  /* back to lib. default resolution */
+  myBH1750.setResolution(BH1750_CONTINUOUS_HIGH_RES_MODE_2);
+}
