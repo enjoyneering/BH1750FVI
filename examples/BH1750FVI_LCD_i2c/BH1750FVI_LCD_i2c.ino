ba06af9628972a48e5d33168022667caa944f799
enjoyneering <enjoyneering79@gmail.com>
2018-06-17 21:53:54 -0400

    Update BH1750FVI_LCD_i2c.ino

--- examples/BH1750FVI_LCD_i2c/BH1750FVI_LCD_i2c.ino
+++ examples/BH1750FVI_LCD_i2c/BH1750FVI_LCD_i2c.ino
@@ -8,24 +8,32 @@
   Wide range:           1 - 65'535 lx (by default)
   Possible to detect:   0.11 - 100'000 lx by changing Sensitivity
 
-  This sensor uses I2C bus to communicate, specials pins are required to interface
+  This sketch uses I2C bus to communicate, specials pins are required to interface
+  Board:                                    SDA                    SCL
+  Uno, Mini, Pro, ATmega168, ATmega328..... A4                     A5
+  Mega2560, Due............................ 20                     21
+  Leonardo, Micro, ATmega32U4.............. 2                      3
+  Digistump, Trinket, ATtiny85............. 0/physical pin no.5    2/physical pin no.7
+  Blue Pill, STM32F103xxxx boards.......... PB7*                   PB6*
+  ESP8266 ESP-01:.......................... GPIO0/D5               GPIO2/D3
+  NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1
 
-  Connect chip to pins:    SDA        SCL
-  Uno, Mini, Pro:          A4         A5
-  Mega2560, Due:           20         21
-  Leonardo:                2          3
-  ATtiny85:                0(5)       2/A1(7)   (ATTinyCore  - https://github.com/SpenceKonde/ATTinyCore
-                                                 & TinyWireM - https://github.com/SpenceKonde/TinyWireM)
-  ESP8266 ESP-01:          GPIO0/D5   GPIO2/D3  (ESP8266Core - https://github.com/esp8266/Arduino)
-  NodeMCU 1.0:             GPIO4/D2   GPIO5/D1
-  WeMos D1 Mini:           GPIO4/D2   GPIO5/D1
+                                            *STM32F103xxxx pins B7/B7 are 5v tolerant, but bi-directional
+                                             logic level converter is recommended
 
-  BSD license, all text above must be included in any redistribution
+  Frameworks & Libraries:
+  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
+  ESP8266 Core          - https://github.com/esp8266/Arduino
+  ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
+  STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
+
+  GNU GPL license, all text above must be included in any redistribution, see link below for details
+  - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
-#include <Wire.h>
+#include <Wire.h>                //https://github.com/enjoyneering/ESP8266-I2C-Driver
 #include <BH1750FVI.h>
-#include <LiquidCrystal_I2C.h> //https://github.com/enjoyneering/LiquidCrystal_I2C
+#include <LiquidCrystal_I2C.h>   //https://github.com/enjoyneering/LiquidCrystal_I2C
 
 #define LCD_ROWS           4     //qnt. of lcd rows
 #define LCD_COLUMNS        20    //qnt. of lcd columns
@@ -34,8 +42,9 @@
 #define LIGHT_LEVEL_OFFICE 500   //normal office light level, lx
 #define PLANTS_GROW        12000 //recommended indoor light level for plants growing, 12000 – 20000 lx
 
-uint8_t in_power_two[8] = {0x1C, 0x04, 0x08, 0x10, 0x1C, 0x00, 0x00, 0x00};
-float   lightLevel      = 0;
+const uint8_t in_power_two[8] PROGMEM = {0x1C, 0x04, 0x08, 0x10, 0x1C, 0x00, 0x00, 0x00}; //PROGMEM saves variable to flash & keeps dynamic memory free
+
+float lightLevel = 0;
 
 /*
 BH1750FVI(address, resolution, sensitivity)
@@ -69,25 +78,30 @@ void setup()
   Serial.begin(115200);
 
   /* LCD connection check */  
-  while (lcd.begin(LCD_COLUMNS, LCD_ROWS, LCD_5x8DOTS) != true) //20x4 display, LCD_5x8DOTS pixels size, SDA - D2, SCL - D1
+  while (lcd.begin(LCD_COLUMNS, LCD_ROWS, LCD_5x8DOTS) != true) //20x4 display, 5x8 pixels size
   {
-    Serial.println("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.");
+    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.")); //PROGMEM saves variable to flash & keeps dynamic memory free
     delay(5000);
   }
-  lcd.print("PCF8574 is OK");
+
+  lcd.print(F("PCF8574 is OK"));
   delay(1000);
+
   lcd.clear();
 
   /* BH1750 connection check */
   while (myBH1750.begin() != true)
   {
     lcd.setCursor(0, 0);
-    lcd.print("BH1750 error");
+    lcd.print(F("BH1750 error"));
     delay(5000);
   }
+
   lcd.clear();
-  lcd.print("BH1750 OK");
+
+  lcd.print(F("BH1750 OK"));
   delay(2000);
+
   lcd.clear();
 
   /* load custom symbol to CGRAM */
@@ -95,10 +109,10 @@ void setup()
 
   /* prints static text */
   lcd.setCursor(0, 0);
-    lcd.print("Light:"); 
+  lcd.print(F("Light:")); 
 
   lcd.setCursor(0, 1);
-    lcd.print("Power:");
+  lcd.print(F("Power:"));
 }
 
 void loop()
@@ -106,17 +120,17 @@ void loop()
   lightLevel = myBH1750.readLightLevel();
 
   lcd.setCursor(6, 0);
-    lcd.print(lightLevel);
-    lcd.write(LCD_SPACE_SYMBOL);
-    lcd.print("lx");
-    lcd.write(LCD_SPACE_SYMBOL);
+  lcd.print(lightLevel);
+  lcd.write(LCD_SPACE_SYMBOL);
+  lcd.print(F("lx"));
+  lcd.write(LCD_SPACE_SYMBOL);
 
   lcd.setCursor(6, 1);
-    lcd.print(lightLevel/683);
-    lcd.write(LCD_SPACE_SYMBOL);
-    lcd.print("Watt/m");
-    lcd.write((uint8_t)0);
-    lcd.write(LCD_SPACE_SYMBOL);
+  lcd.print(lightLevel/683);
+  lcd.write(LCD_SPACE_SYMBOL);
+  lcd.print(F("Watt/m"));
+  lcd.write((uint8_t)0);
+  lcd.write(LCD_SPACE_SYMBOL);
 
   lcd.printHorizontalGraph('L', 2, lightLevel, LIGHT_LEVEL_OFFICE); //name of the bar, first  row, current value, max. value
   lcd.printHorizontalGraph('G', 3, lightLevel, PLANTS_GROW);        //name of the bar, row, current value, max. value
