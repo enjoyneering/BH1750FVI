0fb3c644c9f3f3210f183c4d03f4dcf6743c0ce1
enjoyneering <enjoyneering79@gmail.com>
2018-08-01 16:50:11 -0400

    Update BH1750FVI.h

--- src/BH1750FVI.h
+++ src/BH1750FVI.h
@@ -3,7 +3,7 @@
   This is an Arduino library for the ROHM BH1750FVI Ambient Light Sensor
 
   Power supply voltage: 2.4 - 3.6v
-  Defaul range:         1 - 65'535 lux
+  Default range:        1 - 65'535 lux
   Measurement accuracy: ±20%, possible to calibrate
   Peak wave length:     560nm, yellow-green
 
